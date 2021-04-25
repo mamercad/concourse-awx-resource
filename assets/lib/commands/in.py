26dd767ad328ece5819b248b469df748358aea78
Mike Pontillo <mpontillo@digitalocean.com>
2021-04-25 11:42:16 -0700

    reformat Python clode with 'black'

--- assets/lib/commands/in.py
+++ assets/lib/commands/in.py
@@ -8,10 +8,7 @@ def main():
     # _stdin = "".join(sys.stdin.readlines())
     # _in = json.loads(_stdin)
 
-    _out = {
-        "version": {},
-        "metadata": [],
-    }
+    _out = {"version": {}, "metadata": []}
 
     print(json.dumps(_out))
 
